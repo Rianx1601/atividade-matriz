e489932821e7d27b429e0bf0f9e2a3372bc62fa9
Rianx1601 <115299227+Rianx1601@users.noreply.github.com>
2023-03-09 14:20:51 -0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -10,12 +10,11 @@ int main(){
 	int mb[3][3];
 	
 	for(int i = 0; i < 3 ; i++)
-		for(int j = 0; j<3 ; j++) {
+		for(int j = 0; j < 3 ; j++) {
 			mb[i][j] = ma[j][i];
 		}
-for(int i = 0; i < 3;i++)
-	for(int j = 0; j < 3 ; j++){
-		printf("%d .",mb[i][j]);
-	}
+	printf("%d,%d,%d\n",mb[0][0],mb[0][1],mb[0][2]);
+	printf("%d,%d,%d\n",mb[1][0],mb[1][1],mb[1][2]);
+	printf("%d,%d,%d\n",mb[2][0],mb[2][1],mb[2][2]);
 	return 0;
 }
