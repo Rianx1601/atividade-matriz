db876d4b098ea5ed6b739ab4a082dbc2d17b6dce
Rianx1601 <115299227+Rianx1601@users.noreply.github.com>
2023-03-09 14:08:28 -0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	
+	int m1[2][2]={{1,2},
+			      {3,4}};
+	int m2[2][2]={{5,6},
+				  {7,8}};
+	int mr[2][2];
+	
+	for(int i = 0; i < 2 ; i++)
+		for(int j = 0; j < 2 ; j++) 
+		{
+			mr[i][j] = 0;
+			for(int k = 0; k <2; k++){
+				mr[i][j] += m1[i][k]*m2[k][j];
+			}
+		}
+	
+		printf("%d,%d\n",mr[0][0],mr[0][1]);
+		printf("%d,%d\n",mr[1][0],mr[1][1]);
+	
+	return 0;
+}
